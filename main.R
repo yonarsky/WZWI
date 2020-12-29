df81d4674eae9f05768f0743916139cafbf0eb0b
yonarsky <adrianjnarski@gmail.com>
2020-12-30 00:35:27 +0100

    first commit

---
+++ main.R
@@ -0,0 +1,28 @@
+# Title     : Aplikacja webowa do akwizycji i analizy danych z artykułów z konferencji Petri Nets and Software Engineering 2017 umieszczonych na portalu ceur-ws.org
+# Created by: yonarsky
+# Created on: 20.12.2020
+
+if(require(shiny)){
+  library(wordcloud2)
+   # Global variables can go here
+   n <- 1
+
+   # Define the UI
+   ui <- bootstrapPage(
+      numericInput('size', 'Size of wordcloud', n),
+     wordcloud2Output('wordcloud2')
+   )
+
+
+   # Define the server code
+   server <- function(input, output) {
+      output$wordcloud2 <- renderWordcloud2({
+        wordcloud2(demoFreq, size=input$size)
+        #wordcloud2(d, size=input$size)
+      })
+   }
+   # Return a Shiny app object
+   # Sys.setlocale("LC_CTYPE","chs") #if you use Chinese character
+   ## Do not Run!
+   shinyApp(ui = ui, server = server)
+   }
